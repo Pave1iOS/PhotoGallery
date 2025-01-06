bbb1ecdd39e79381951c9d5386b1d132db5a9383
Pave1iOS <PGribachev94@yandex.ru>
2025-01-06 11:24:17 +0300

    chapter 24 + retrofit + fragment

--- app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryActivity.kt
@@ -1,15 +1,21 @@
 package com.example.photogallery
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class PhotoGalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_photo_gallery)
+
+        // проверка, размещен ли контейнер фрагмента
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
     }
 }
