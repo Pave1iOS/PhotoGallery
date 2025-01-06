20a03923422072e8a7cf0b838f0bb3bec86e9aaa
Pave1iOS <PGribachev94@yandex.ru>
2025-01-06 15:22:01 +0300

    chapter 24 + network layer

---
+++ app/src/main/java/com/example/photogallery/FlickrFetchr.kt
@@ -0,0 +1,46 @@
+package com.example.photogallery
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+class FlickrFetcher {
+
+    private val flickrApi: FlickrApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    fun fetchContents(): LiveData<String> {
+        val responseLD = MutableLiveData<String>()
+        val flickrRequest = flickrApi.fetchContent()
+
+        flickrRequest.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                responseLD.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "failed to fetch photo", t)
+            }
+        })
+
+        return responseLD
+    }
+
+    companion object {
+        private const val TAG = "FlickrFetcher"
+    }
+}
--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -8,12 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.photogallery.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class PhotoGalleryFragment: Fragment() {
 
@@ -35,28 +29,13 @@ class PhotoGalleryFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
+        val flickrLD = FlickrFetcher().fetchContents()
+        flickrLD.observe(this) {
+            Log.d(TAG, "response received $it")
+        }
 
-        val flickrApi = retrofit.create(FlickrApi::class.java)
-        val flickrHomePageRequest = flickrApi.fetchContent()
 
-        networkRequest(flickrHomePageRequest)
-    }
 
-    private fun networkRequest(call: Call<String>) {
-        call.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                Log.d(TAG, "response received ${response.body()}")
-            }
-
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.e(TAG, "failed to fetch photo", t)
-            }
-
-        })
     }
 
     companion object {
