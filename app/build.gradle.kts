771468a6245f158888b063e3f5db173aaf9a558a
Pave1iOS <PGribachev94@yandex.ru>
2025-01-06 14:39:41 +0300

    chapter 24 + ScalarsConverterFactory

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
     implementation(libs.retrofit)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,12 +1,19 @@
 package com.example.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class PhotoGalleryFragment: Fragment() {
 
@@ -25,7 +32,35 @@ class PhotoGalleryFragment: Fragment() {
         return view
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        val flickrApi = retrofit.create(FlickrApi::class.java)
+        val flickrHomePageRequest = flickrApi.fetchContent()
+
+        networkRequest(flickrHomePageRequest)
+    }
+
+    private fun networkRequest(call: Call<String>) {
+        call.enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "response received ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "failed to fetch photo", t)
+            }
+
+        })
+    }
+
     companion object {
+        private const val TAG = "PhotoGalleryFragment"
         fun newInstance(): PhotoGalleryFragment {
 
             return PhotoGalleryFragment()
