ad92eb87e8398489895eff6d78c81452e2d8db49
Pave1iOS <PGribachev94@yandex.ru>
2025-01-27 23:44:48 +0300

    chapter 25 completed

--- app/src/main/java/com/example/photogallery/photoGalleryFragment/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/photoGalleryFragment/PhotoGalleryViewModel.kt
@@ -1,5 +1,9 @@
 package com.example.photogallery.photoGalleryFragment
 
+import android.content.res.Resources
+import android.graphics.drawable.BitmapDrawable
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,19 +13,31 @@ import com.example.photogallery.data.ThumbnailDownloader
 import javax.inject.Inject
 
 class PhotoGalleryViewModel @Inject constructor(
-    flickrFetcher: FlickrFetcher
+    private val flickrFetcher: FlickrFetcher
 ): ViewModel() {
 
     val galleryItems = flickrFetcher.getPagingPhoto().cachedIn(viewModelScope)
 
-    private var thumbnailDownloader: ThumbnailDownloader<PhotoGalleryAdapter.PhotoGalleryViewHolder> =
-        ThumbnailDownloader(flickrFetcher)
+    private val responseHandler = Handler(Looper.getMainLooper())
+    private lateinit var thumbnailDownloader:
+            ThumbnailDownloader<PhotoGalleryAdapter.PhotoGalleryViewHolder>
 
     private var pictureArray = mutableSetOf<String>()
 
     init {
         Log.i(TAG, "view model is initialization ✅")
+        thumbnailDownloaderInitialize()
+
         thumbnailDownloader.startThread()
+    }
+
+    private fun thumbnailDownloaderInitialize() {
+
+        thumbnailDownloader = ThumbnailDownloader(flickrFetcher, responseHandler) { holder, bitmap ->
+            val drawable = BitmapDrawable(Resources.getSystem(), bitmap)
+            holder.bindDrawable(drawable)
+        }
+
     }
 
     fun downloadPicture(holder: PhotoGalleryAdapter.PhotoGalleryViewHolder, url: String) {
